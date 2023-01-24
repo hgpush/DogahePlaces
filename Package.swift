83ed0e549de8d76a5fca7da9a323a10238ffe17a
dogahe <dogahe@google.com>
2023-01-24 13:06:14 -0500

    new structure

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "DogahePlaces",
+    name: "GooglePlaces",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "DogahePlaces",
-            targets: ["DogahePlaces", "GooglePlaces"]),
+            name: "GooglePlaces",
+            targets: ["GooglePlacesTarget"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,11 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "DogahePlaces",
-            dependencies: []),
+            name: "GooglePlacesTarget",
+            dependencies: [.target(name: "GooglePlaces")],
+            path: "GooglePlacesTarget"
+        ),
         .binaryTarget(name: "GooglePlaces", url: "https://github.com/dogahe/DogahePlaces/releases/download/1.0.0/GooglePlaces.zip", checksum: "d4c327d51a1be2ffd2dbefea9e369205c1103023956eaa36ad72e625d6fa939f"),
-        .testTarget(
-            name: "DogahePlacesTests",
-            dependencies: ["DogahePlaces"]),
     ]
 )
