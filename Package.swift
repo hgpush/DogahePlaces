57e856636dab8b7621d38d2f39a6b653ed083758
dogahe <dogahe@google.com>
2023-01-30 15:07:16 -0500

    save

--- Package.swift
+++ Package.swift
@@ -23,6 +23,8 @@ let package = Package(
             dependencies: [.target(name: "GooglePlaces")],
             path: "GooglePlacesTarget"
         ),
-        .binaryTarget(name: "GooglePlaces", url: "https://github.com/dogahe/DogahePlaces/releases/download/1.0.0/GooglePlaces.zip", checksum: "d4c327d51a1be2ffd2dbefea9e369205c1103023956eaa36ad72e625d6fa939f"),
+        .binaryTarget(name: "GooglePlaces", url: "https://github.com/dogahe/DogahePlaces/releases/download/1.0.0/GooglePlaces.zip", checksum: "bb4705e64801cf2bfb644c324c73ca3901d5eefadcec34a84d2144592f79edc8"),
     ]
 )
+
+// d4c327d51a1be2ffd2dbefea9e369205c1103023956eaa36ad72e625d6fa939f
