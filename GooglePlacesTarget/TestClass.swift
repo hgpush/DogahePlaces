36b1bb6e2c1c234e6b6cbcca353866af378b538d
Pushkar Sharma <gitpush@google.com>
2023-02-13 16:07:50 -0800

    restructure to swift source

---
+++ GooglePlacesTarget/TestClass.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Pushkar Sharma on 2/13/23.
+//
+
+import Foundation
+
+class TestClass {
+  let imageURL = Bundle.module.url(forResource: "ss", withExtension: "png")
+}
