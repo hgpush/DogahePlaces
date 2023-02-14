49ad176b4463c4503562c6d35a9c3f9ce4e2a4d0
Pushkar Sharma <gitpush@google.com>
2023-02-13 16:20:28 -0800

    add public func

--- GooglePlacesTarget/GooglePlacesTarget.swift
+++ GooglePlacesTarget/GooglePlacesTarget.swift
@@ -9,4 +9,8 @@ import Foundation
 
 struct GooglePlacesTarget {
   let imageURL = Bundle.module.url(forResource: "ss", withExtension: "png")
+  
+  public func getImageUrl() -> URL? {
+    return imageURL
+  }
 }
